281bd2e463e7f28a92b69f5e035bc02a70cebb60
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-03 16:08:25 +0530

    new code

---
+++ 4.Print Ascii Value using For Loop(example-4).cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=256;++i)
+    {
+        cout<<char(i);
+    }
+}
---
+++ 6.Find the power of any no. using for loop (example-6).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=2;
+    int power=1;
+    for(int i=1;i<=3;++i)
+    {
+        power=power*a;
+    }
+    cout<<power;
+}
